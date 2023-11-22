8b1e9ba68035efcf3e0c7d3c160ddb05df7735b4
GitMurf <64155612+GitMurf@users.noreply.github.com>
2023-11-22 08:40:55 -0800

    tests: add types.ts placeholder for vitest setup files

---
+++ __tests__/__setupFiles__/types.ts
@@ -0,0 +1,14 @@
+// type NodeJsGlobal = NodeJS.Global;
+// export interface PluginNodeGlobal extends NodeJsGlobal {
+//   myTest: number;
+// }
+
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       document: Document;
+//       window: Window;
+//       navigator: Navigator;
+//     }
+//   }
+// }
